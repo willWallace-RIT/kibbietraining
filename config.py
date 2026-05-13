87b58c53e4667da8655b889cae631f80a7a86ded
northbot <54906390+willWallace-RIT@users.noreply.github.com>
2026-05-13 16:38:37 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+CAMERA_INDEX = 0
+FRAME_WIDTH = 1280
+FRAME_HEIGHT = 720
+
+KEYBOARD_LAYOUT = "qwerty"
+
+GREEN_THRESHOLD = {
+    "h_min": 35,
+    "h_max": 85,
+    "s_min": 80,
+    "v_min": 50
+}
+
+MODEL_PATH = "model/weights.pth"
+
+SMOOTHING_WINDOW = 5
+PRESS_THRESHOLD = 0.72
